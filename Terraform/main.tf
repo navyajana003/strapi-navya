0bb0929347c4a1cf738ccee26565144791fc0565
Navya <navya.jana1999@gmail.com>
2025-07-22 22:26:40 +0530

    udpated ci.yml

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
  
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-app-sg-navya"
+  name        = "strapi-sg-navya"
   description = "Allow SSH and Strapi"
  
   ingress {
